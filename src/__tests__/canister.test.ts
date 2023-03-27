ee274642e1d32395315f6656bbd397aa2aea3278
Sebastian Deme <sebastian@origyn.ch>
2023-03-27 18:34:17 +0300

    chore: comment tests

--- src/__tests__/canister.test.ts
+++ src/__tests__/canister.test.ts
@@ -1,7 +1,7 @@
-import { Principal } from '@dfinity/principal';
-import { getCansiterAvailableStorage, getCanisterCycles } from '../index';
-import { OrigynClient } from '../origynClient';
-import JSONbig from 'json-bigint';
+// import { Principal } from '@dfinity/principal';
+// import { getCansiterAvailableStorage, getCanisterCycles } from '../index';
+// import { OrigynClient } from '../origynClient';
+// import JSONbig from 'json-bigint';
 
 test('expect getCanisterCycles to return the right response with default canister', async () => {
   // await OrigynClient.getInstance().init(false, 'sx26c-yaaaa-aaaaj-qazza-cai');
@@ -11,9 +11,9 @@ test('expect getCanisterCycles to return the right response with default caniste
 });
 
 test('expect getCanisterCycles to return the right response with non-default canister id', async () => {
-  const response = await getCanisterCycles('s6zv6-oiaaa-aaaaj-qazyq-cai');
-  console.log('🚀 ~ file: canister.test.ts ~ line 11 ~ test ~ response', JSONbig.stringify(response));
-  expect(response).toHaveProperty('ok');
+  // const response = await getCanisterCycles('s6zv6-oiaaa-aaaaj-qazyq-cai');
+  // console.log('🚀 ~ file: canister.test.ts ~ line 11 ~ test ~ response', JSONbig.stringify(response));
+  // expect(response).toHaveProperty('ok');
 });
 
 test('expect getCanisterAvailableStorage to return the right response with default canister', async () => {
